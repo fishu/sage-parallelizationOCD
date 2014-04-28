ea711aa4c0e27f3ab06a607ab93eb2c54c2b65ea
mephistorockshard <shouhengyi@gmail.com>
2014-04-28 15:58:06 -0700

    Create test.c

---
+++ stats/base/c/test.c
@@ -0,0 +1,9 @@
+/*	Logs:
+*	Author(s): Shouheng Yi
+*	Created: 04/28/2014
+*	Updated: 04/28/2014
+*
+*	References:
+*	Numerically Stable, Single-Pass, Parallel Statistics Algorithms
+*	http://www.janinebennett.org/index_files/ParallelStatisticsAlgorithms.pdf
+*/
